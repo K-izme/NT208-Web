d060c63601556928252f172e60f30c88e189d1a4
kietngo255 <21521034@gm.uit.edu.vn>
2023-09-19 18:10:37 +0700

    frontend3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,15 +3,21 @@ import './custom.css'
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Container } from 'react-bootstrap';
 
 import img1 from '../../images/img1.svg';
-import backgroundimg from '../../images/leftimagesbackground.svg';
-import { Container } from 'react-bootstrap';
+import img2 from '../../images/img2.svg';
+import star1 from '../../images/star.svg';
+import star2 from '../../images/star2.svg';
+import leftbackgroundimg from '../../images/leftimagesbackground.svg';
+import rightbackgroundimg from '../../images/rightimagesbackground.svg';
+import middlebackgroundimg from '../../images/middleimagesbackground.svg';
 
 function Home () {
     const backgroundColor = {
         backgroundColor: "#0E1620",
         height: "7432px",
+        width: "100%",
         zIndex: -2
       };
 
@@ -52,7 +58,7 @@ function Home () {
 
 
             <Row className="HomeStart">
-                <div>
+                <Col className='Limit'>
                     <h2 className="Title title">
                         Code <span style={{ color: "#21B573" }}>Lord</span>
                         <br />
@@ -60,13 +66,25 @@ function Home () {
                     </h2>
                     
                     <div className="background-img1 background-img1">
-                        <Image className="backroundimg1" src={backgroundimg} alt="lung" />
+                        <Image className="backroundimg1" src={leftbackgroundimg} alt="lung" />
                     </div> 
 
                     <div className="left-img left-img">
                         <Image className="img1" src={img1} alt="a-man-using-laptop" />
                     </div> 
-                </div>
+
+                    <div className="background-img2 background-img2">
+                        <Image className="backroundimg2" src={rightbackgroundimg} alt="linh" />
+                    </div> 
+
+                    <div className="right-img right-img">
+                        <Image className="img2" src={img2} alt="a-man-using-laptop-2" />
+                    </div> 
+
+                    <div className="middlebackground background-img3">
+                        <Image className="backgroundimg3" src={middlebackgroundimg} alt="qua" />
+                    </div> 
+                </Col>
 
                 <Col class="position-relative">
                     <div class="col-md-8 justify-content-center">
@@ -77,9 +95,58 @@ function Home () {
                         </div>
                     </div>
                 </Col>
-
             </Row>
 
+            <Container className="Categories">
+                <div className="box">
+                    <Row>
+                    <Col className="group-number-banner">
+                        <Row className="group-student">
+                            <div className="overlap-group">
+                                <div className="rectangle" />
+                                <div className="text-wrapper">100M+</div>
+                                <div className="div">Students</div>
+                            </div>
+                        </Row>
+                        <Row className="group-professional">
+                            <div className="group">
+                                <div className="text-wrapper-2">10K+</div>
+                                <div className="text-wrapper-3">Professionals</div>
+                            </div>
+                        </Row>
+                        <Row className="group-trustpilot">
+                            <div className="overlap-2">
+                                <Row className="overlap-3">
+                                    <div className="text-wrapper-4">5.0</div>
+                                    <div className="group-2">
+                                        <div className="text-wrapper-5">View our 1,602 reviews</div>
+                                        <div className="overlap-group-2">
+                                        <Image className="artboard" src={star1} alt="Artboard" />
+                                        </div>
+                                    </div>
+                                    <h4 className="texttrust">
+                                            Trustpilot
+                                    </h4> 
+                                </Row>
+                                <Image className="star" src={star2} alt="Star" />
+                            </div>
+                        </Row>
+                        <Row className="group-category">
+                            <div className="overlap-4">
+                                <div className="text-wrapper-6">15+</div>
+                                <div className="text-wrapper-7">Category</div>
+                            </div>
+                        </Row>
+                        <Row className="group-livecourses">
+                            <div className="overlap-5">
+                                <div className="text-wrapper-8">60K+</div>
+                                <div className="text-wrapper-9">Live Courses</div>
+                            </div>
+                        </Row>
+                    </Col>
+                    </Row>
+                </div>
+            </Container>
 
 
         </div>
